874b566cf29ab519b55f9c90b39be6df81bee2cf
Jery Lu (CSS) <v-jelu@microsoft.com>
2019-05-24 12:14:08 +0800

    first

---
+++ app/controllers/redis_controller.rb
@@ -0,0 +1,30 @@
+require 'redis'
+require 'date'
+require 'json'
+
+class RedisController < ApplicationController
+    $redis = Redis.new(
+        :host => 'jery-redis.redis.cache.windows.net', 
+        :port => 6380, 
+        :db => 0, 
+        :password => "ErBsK9l0KA9WlduqhlsV0Qd22RGq9B4RFYjb5Uo5F5o=", 
+        :ssl => :true
+    )
+
+    print $redis.ping()
+    print "Connected\n"
+
+    data = {"sender" => "Frank Pan" }
+
+    msg = 'hello'
+
+    loop do
+        sleep 10
+
+        $redis.publish("chat", data.merge('message' => msg).to_json)
+
+        timestamp = DateTime.now.strftime("%d/%m/%Y %H:%M:%S")
+
+        puts "#send '#{msg}' on #{timestamp}"
+    end
+end
---
+++ app/models/redis.rb
@@ -0,0 +1,2 @@
+class Redis < ApplicationRecord
+end
